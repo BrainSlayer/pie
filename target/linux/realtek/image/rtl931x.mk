bb59695e29dde4599125e94bd5a21aba8899875e
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2021-09-26 20:08:40 +0700

    fix merge
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -7,6 +7,19 @@ define Device/edgecore_ecs4125-10p
   IMAGE_SIZE := 14336k
   DEVICE_VENDOR := Edgecore
   DEVICE_MODEL := ECS4125-10P
+  DEVICE_PACKAGES := ip-bridge ethtool
   DEVICE_PACKAGES += lua-rs232
 endef
 TARGET_DEVICES += edgecore_ecs4125-10p
+
+define Device/zyxel_xs1900-10
+  KERNEL_LOADADDR = 0x80220000
+  KERNEL_ENTRY = 0x80220000
+  SOC := rtl9313
+  IMAGE_SIZE := 14336k
+  DEVICE_MODEL := XS1900-10
+  KERNEL := kernel-bin | append-dtb | gzip | custom-uimage OpenWRT
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | gzip | custom-uimage OpenWRT
+  DEVICE_PACKAGES := ip-bridge ethtool
+endef
+TARGET_DEVICES += zyxel_xs1900-10
