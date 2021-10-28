bcd48c4a1ad1f2c309b14795accc5f023b92a686
Birger Koblitz <git@birger-koblitz.de>
2021-10-28 21:05:29 +0200

    realtek: Treate RTL9300 CEVT same as CEVT-R4K
    Makes sure the timer is called in the same way the R4K timer is called.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>

---
+++ target/linux/realtek/files-5.10/arch/mips/include/asm/time.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2001, 2002, MontaVista Software Inc.
+ * Author: Jun Sun, jsun@mvista.com or jsun@junsun.net
+ * Copyright (c) 2003  Maciej W. Rozycki
+ *
+ * include/asm-mips/time.h
+ *     header file for the new style time.c file and time services.
+ */
+#ifndef _ASM_TIME_H
+#define _ASM_TIME_H
+
+#include <linux/rtc.h>
+#include <linux/spinlock.h>
+#include <linux/clockchips.h>
+#include <linux/clocksource.h>
+
+extern void rtl9300_clockevent_init(void);
+
+extern spinlock_t rtc_lock;
+
+/*
+ * board specific routines required by time_init().
+ */
+extern void plat_time_init(void);
+
+/*
+ * mips_hpt_frequency - must be set if you intend to use an R4k-compatible
+ * counter as a timer interrupt source.
+ */
+extern unsigned int mips_hpt_frequency;
+
+/*
+ * The performance counter IRQ on MIPS is a close relative to the timer IRQ
+ * so it lives here.
+ */
+extern int (*perf_irq)(void);
+extern int __weak get_c0_perfcount_int(void);
+
+/*
+ * Initialize the calling CPU's compare interrupt as clockevent device
+ */
+extern unsigned int get_c0_compare_int(void);
+extern int r4k_clockevent_init(void);
+
+static inline int mips_clockevent_init(void)
+{
+	pr_info("%s: ################## 1\n", __func__);
+#ifdef CONFIG_CEVT_RTL9300
+	rtl9300_clockevent_init();
+	return 0;
+#endif
+
+#ifdef CONFIG_CEVT_R4K
+	pr_info("%s: ################## 2\n", __func__);
+	return r4k_clockevent_init();
+#else
+	pr_info("%s: ################## 3\n", __func__);
+	return -ENXIO;
+#endif
+}
+
+/*
+ * Initialize the count register as a clocksource
+ */
+extern int init_r4k_clocksource(void);
+
+static inline int init_mips_clocksource(void)
+{
+	pr_info("%s: ################## 1\n", __func__);
+#ifdef CONFIG_CSRC_R4K
+	pr_info("%s: ################## 2\n", __func__);
+	return init_r4k_clocksource();
+#else
+	pr_info("%s: ################## 3\n", __func__);
+	return 0;
+#endif
+}
+
+static inline void clockevent_set_clock(struct clock_event_device *cd,
+					unsigned int clock)
+{
+	clockevents_calc_mult_shift(cd, clock, 4);
+}
+
+#endif /* _ASM_TIME_H */
